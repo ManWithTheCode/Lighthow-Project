b98ee7f8316919e76f74e1b3ba158139623d3a05
ManWithTheCode <113933694+ManWithTheCode@users.noreply.github.com>
2022-09-26 13:34:11 -0400

    Update Lightshow.py

--- Lightshow.py
+++ Lightshow.py
@@ -1,2 +1,6 @@
 # Lighthow-Project
-Create a lightshow for the Jack-o-lantern.  It must use multiple colors that are associated with the holiday.  It must demonstrate multiple lighting effects (at least 5 different ones).  
+'''
+Create a lightshow for the Jack-o-lantern.  
+It must use multiple colors that are associated with the holiday. It must demonstrate multiple lighting effects (at least 5 different ones).  
+'''
+
